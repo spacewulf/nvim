c66202498248cb1512e19c768825773a77756e07
spacewulf <kwolterstorff531@gmail.com>
2024-10-19 21:22:03 -0400

    Fixed a typo in lspconfig.lua within the plugins folder.

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,12 +1,12 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile", "User FilePost" },
-    dependencies = {
-      'nvim-lua/lsp-status.nvim',
-    }
-    config = function()
-      require('config.lspconfig')
-    end
-  }
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile", "User FilePost" },
+		dependencies = {
+			"nvim-lua/lsp-status.nvim",
+		},
+		config = function()
+			require("config.lspconfig")
+		end,
+	},
 }
