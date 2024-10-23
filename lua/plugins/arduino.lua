c83fe074ad9df12fd1a1617ad325bf1c6f46e2b7
spacewulf <kwolterstorff531@gmail.com>
2024-10-23 16:10:32 -0400

    Added arduino coding support

---
+++ lua/plugins/arduino.lua
@@ -0,0 +1,26 @@
+return {
+	{
+		"stevearc/vim-arduino",
+		config = function()
+			require("config.arduino")
+		end,
+		cmd = {
+			"ArduinoAttach",
+			"ArduinoVerify",
+			"ArduinoVerify",
+			"ArduinoUploadAndSerial",
+			"ArduinoSerial",
+			"ArduinoChooseBoard",
+			"ArduinoChooseProgrammer",
+		},
+		keys = {
+			{ "<leader>aa", "<cmd>ArduinoAttach<cr>" },
+			{ "<leader>av", "<cmd>ArduinoVerify<cr>" },
+			{ "<leader>au", "<cmd>ArduinoUpload<cr>" },
+			{ "<leader>aus", "<cmd>ArduinoUploadAndSerial<cr>" },
+			{ "<leader>as", "<cmd>ArduinoSerial<cr>" },
+			{ "<leader>ab", "<cmd>ArduinoChooseBoard<cr>" },
+			{ "<leader>ap", "<cmd>ArduinoChooseProgrammer<cr>" },
+		},
+	},
+}
