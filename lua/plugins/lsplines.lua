3d99d238a4aa59ad9ebd5af63f2b5b577f8d3723
spacewulf <kwolterstorff531@gmail.com>
2024-10-30 10:26:00 -0400

    Updated LSPLines with new configuration.

--- lua/plugins/lsplines.lua
+++ lua/plugins/lsplines.lua
@@ -1,10 +1,11 @@
 return {
-  {
-    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-    config = function()
-      require("lsp_lines").setup()
-    end,
-    dependencies = { "neovim/nvim-lspconfig" },
-    enabled = false
-  }
+	{
+		"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+		config = function()
+			require("lsp_lines").setup()
+			vim.diagnostic.config({ virtual_lines = false }, require("lazy.core.config").ns)
+		end,
+		dependencies = { "neovim/nvim-lspconfig" },
+		enabled = true,
+	},
 }
--- lua/plugins/tmux-navigator.lua
+++ lua/plugins/tmux-navigator.lua
@@ -1,19 +1,9 @@
 return {
 	{
-		"christoomey/vim-tmux-navigator",
-		cmd = {
-			"TmuxNavigateLeft",
-			"TmuxNavigateDown",
-			"TmuxNavigateUp",
-			"TmuxNavigateRight",
-			"TmuxNavigatePrevious",
-		},
-		keys = {
-			{ "<c-h>", "<cmd<C-U>TmuxNavigateLeft<cr>" },
-			{ "<c-j>", "<cmd<C-U>TmuxNavigateDown<cr>" },
-			{ "<c-k>", "<cmd<C-U>TmuxNavigateUp<cr>" },
-			{ "<c-l>", "<cmd<C-U>TmuxNavigateRight<cr>" },
-			{ "<c-\\>", "<cmd<C-U>TmuxNavigatePrevious<cr>" },
-		},
+		"alexghergh/nvim-tmux-navigation",
+		config = function()
+			require("config.tmux-navigator")
+		end,
+		opts = {},
 	},
 }
