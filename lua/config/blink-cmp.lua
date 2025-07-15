1d88ecbaab33334b396d52292dff2a49b26556ba
spacewulf <kwolterstorff531@gmail.com>
2025-07-15 14:58:43 -0400

    Updated to new configs and new versions of some plugins

--- lua/config/blink-cmp.lua
+++ lua/config/blink-cmp.lua
@@ -7,69 +7,69 @@ require("blink.cmp").setup({
 		["<C-space>"] = { "show", "show_documentation", "fallback" },
 		["<C-e>"] = { "hide", "fallback" },
 	},
-	completion = {
-		--[[
-		list = {
-			selection = function(ctx)
-				return ctx.mode == "cmdline" and "auto_insert" or "preselect"
-			end,
-		},
-    ]]
-		--
-		keyword = { range = "full" },
-		accept = { auto_brackets = { enabled = false } },
-		menu = {
-			auto_show = true,
-
-			draw = {
-				columns = {
-					{ "label", "label_description", gap = 1 },
-					{ "kind_icon", "kind" },
-				},
-			},
-		},
-		documentation = { auto_show = true },
-		ghost_text = { enabled = true },
-	},
-	sources = {
-		default = { "lsp", "path", "snippets", "buffer" },
-		providers = {
-			lsp = {
-				name = "lsp",
-				enabled = true,
-				module = "blink.cmp.sources.lsp",
-				score_offset = 1000,
-			},
-			snippets = {
-				name = "snippets",
-				enabled = true,
-				module = "blink.cmp.sources.snippets",
-				score_offset = 900,
-			},
-			path = {
-				name = "path",
-				enabled = true,
-				module = "blink.cmp.sources.path",
-				fallbacks = { "buffer" },
-				opts = {
-					trailing_slash = false,
-					label_trailing_slash = true,
-					get_cwd = function(context)
-						return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
-					end,
-					show_hidden_files_by_default = true,
-				},
-				score_offset = 900,
-			},
-			buffer = {
-				name = "buffer",
-				enabled = true,
-				max_items = 3,
-				module = "blink.cmp.sources.buffer",
-				min_keyword_length = 4,
-			},
-		},
-		--[[
+	-- completion = {
+	-- 	--[[
+	-- 	list = {
+	-- 		selection = function(ctx)
+	-- 			return ctx.mode == "cmdline" and "auto_insert" or "preselect"
+	-- 		end,
+	-- 	},
+	--    ]]
+	-- 	--
+	-- 	keyword = { range = "full" },
+	-- 	accept = { auto_brackets = { enabled = false } },
+	-- 	menu = {
+	-- 		auto_show = true,
+	--
+	-- 		draw = {
+	-- 			columns = {
+	-- 				{ "label", "label_description", gap = 1 },
+	-- 				{ "kind_icon", "kind" },
+	-- 			},
+	-- 		},
+	-- 	},
+	-- 	documentation = { auto_show = true },
+	-- 	ghost_text = { enabled = true },
+	-- },
+	-- sources = {
+	-- 	default = { "lsp", "path", "snippets", "buffer" },
+	-- 	providers = {
+	-- 		lsp = {
+	-- 			name = "lsp",
+	-- 			enabled = true,
+	-- 			module = "blink.cmp.sources.lsp",
+	-- 			score_offset = 1000,
+	-- 		},
+	-- 		snippets = {
+	-- 			name = "snippets",
+	-- 			enabled = true,
+	-- 			module = "blink.cmp.sources.snippets",
+	-- 			score_offset = 900,
+	-- 		},
+	-- 		path = {
+	-- 			name = "path",
+	-- 			enabled = true,
+	-- 			module = "blink.cmp.sources.path",
+	-- 			fallbacks = { "buffer" },
+	-- 			opts = {
+	-- 				trailing_slash = false,
+	-- 				label_trailing_slash = true,
+	-- 				get_cwd = function(context)
+	-- 					return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
+	-- 				end,
+	-- 				show_hidden_files_by_default = true,
+	-- 			},
+	-- 			score_offset = 900,
+	-- 		},
+	-- 		buffer = {
+	-- 			name = "buffer",
+	-- 			enabled = true,
+	-- 			max_items = 3,
+	-- 			module = "blink.cmp.sources.buffer",
+	-- 			min_keyword_length = 4,
+	-- 		},
+	-- 	},
+	--[[
 		cmdline = function()
 			local type = vim.fn.getcmdtype()
 			if type == "/" or type == "?" then
@@ -81,6 +81,5 @@ require("blink.cmp").setup({
 			return {}
 		end,
     ]]
-		--
-	},
+	--
 })
--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -2,15 +2,17 @@ local mason_lspconfig = require("mason-lspconfig")
 
 -- local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-mason_lspconfig.setup()
-
-mason_lspconfig.setup_handlers({
-	function(server_name)
-		require("lspconfig")[server_name].setup({
-			capabilities = capabilities,
-		})
-		capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
-	end,
+mason_lspconfig.setup({
+	automatic_enable = true,
 })
+
+-- mason_lspconfig.setup_handlers({
+-- 	function(server_name)
+-- 		require("lspconfig")[server_name].setup({
+-- 			capabilities = capabilities,
+-- 		})
+-- 		capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
+-- 	end,
+-- })
 
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename using the current LSP" })
