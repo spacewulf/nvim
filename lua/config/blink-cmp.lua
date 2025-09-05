d6901dbc3e8987f02ca561371d245d53f4917c5d
spacewulf <kwolterstorff531@gmail.com>
2025-09-05 11:35:30 -0400

    Updated blink config

--- lua/config/blink-cmp.lua
+++ lua/config/blink-cmp.lua
@@ -1,4 +1,4 @@
-require("blink.cmp").setup({
+local blink = require("blink.cmp").setup({
 	keymap = {
 		preset = "default",
 
@@ -7,79 +7,28 @@ require("blink.cmp").setup({
 		["<C-space>"] = { "show", "show_documentation", "fallback" },
 		["<C-e>"] = { "hide", "fallback" },
 	},
-	-- completion = {
-	-- 	--[[
-	-- 	list = {
-	-- 		selection = function(ctx)
-	-- 			return ctx.mode == "cmdline" and "auto_insert" or "preselect"
-	-- 		end,
-	-- 	},
-	--    ]]
-	-- 	--
-	-- 	keyword = { range = "full" },
-	-- 	accept = { auto_brackets = { enabled = false } },
-	-- 	menu = {
-	-- 		auto_show = true,
-	--
-	-- 		draw = {
-	-- 			columns = {
-	-- 				{ "label", "label_description", gap = 1 },
-	-- 				{ "kind_icon", "kind" },
-	-- 			},
-	-- 		},
-	-- 	},
-	-- 	documentation = { auto_show = true },
-	-- 	ghost_text = { enabled = true },
-	-- },
-	-- sources = {
-	-- 	default = { "lsp", "path", "snippets", "buffer" },
-	-- 	providers = {
-	-- 		lsp = {
-	-- 			name = "lsp",
-	-- 			enabled = true,
-	-- 			module = "blink.cmp.sources.lsp",
-	-- 			score_offset = 1000,
-	-- 		},
-	-- 		snippets = {
-	-- 			name = "snippets",
-	-- 			enabled = true,
-	-- 			module = "blink.cmp.sources.snippets",
-	-- 			score_offset = 900,
-	-- 		},
-	-- 		path = {
-	-- 			name = "path",
-	-- 			enabled = true,
-	-- 			module = "blink.cmp.sources.path",
-	-- 			fallbacks = { "buffer" },
-	-- 			opts = {
-	-- 				trailing_slash = false,
-	-- 				label_trailing_slash = true,
-	-- 				get_cwd = function(context)
-	-- 					return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
-	-- 				end,
-	-- 				show_hidden_files_by_default = true,
-	-- 			},
-	-- 			score_offset = 900,
-	-- 		},
-	-- 		buffer = {
-	-- 			name = "buffer",
-	-- 			enabled = true,
-	-- 			max_items = 3,
-	-- 			module = "blink.cmp.sources.buffer",
-	-- 			min_keyword_length = 4,
-	-- 		},
-	-- 	},
-	--[[
-		cmdline = function()
-			local type = vim.fn.getcmdtype()
-			if type == "/" or type == "?" then
-				return { "buffer" }
-			end
-			if type == ":" then
-				return { "cmdline" }
-			end
-			return {}
-		end,
-    ]]
-	--
+	sources = {
+		providers = {
+			buffer = {
+				opts = {
+					get_bufnrs = function()
+						return vim.tbl_filter(function(bufnr)
+							return vim.bo[bufnr].buftype == ""
+						end, vim.api.nvim_list_bufs())
+					end,
+				},
+			},
+		},
+	},
 })
+
+blink.sources.default = function(ctx)
+	local success, node = pcall(vim.treesitter.get_node)
+	if success and node and vim.tbl_contains({ "comment", "line_comment", "block_comment" }, node:type()) then
+		return { "buffer" }
+	elseif vim.bo.filetype == "lua" then
+		return { "lsp", "path" }
+	else
+		return { "lsp", "path", "snippets", "buffer" }
+	end
+end
