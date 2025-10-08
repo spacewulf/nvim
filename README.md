0cd2fb7164167d9a118e6d603a259d3cde2f7172
spacewulf <kwolterstorff531@gmail.com>
2025-10-07 23:06:05 -0400

    Updated with some new lazy loading

---
+++ README.md
@@ -0,0 +1,5 @@
+# Neovim Configuration
+
+- LSP
+- Formatter
+- Autocomplete
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,6 +2,7 @@ return {
 	{
 		"akinsho/toggleterm.nvim",
 		version = "*",
+		event = "VeryLazy",
 		opts = {
 			open_mapping = [[<c-\>]],
 			hide_numbers = true,
