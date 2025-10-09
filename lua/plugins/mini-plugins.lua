2ec7982942e0a9a48894231730c2bf5d30a4ee1d
spacewulf <kwolterstorff531@gmail.com>
2025-10-08 23:55:35 -0400

    Updated mini-plugins.lua so the setup() method is actually called.

--- lua/plugins/mini-plugins.lua
+++ lua/plugins/mini-plugins.lua
@@ -2,13 +2,16 @@ return {
 	{
 		"nvim-mini/mini.pairs",
 		version = "*",
+		opts = {},
 	},
 	{
 		"nvim-mini/mini.surround",
 		version = "*",
+		opts = {},
 	},
 	{
 		"nvim-mini/mini.ai",
 		version = "*",
+		opts = {},
 	},
 }
